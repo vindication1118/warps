adfda7798f3a840367c272abaff42bd8be8b58e1
vindication1118 <vindication1118@gmail.com>
2015-02-27 23:49:54 -0700

    Warps
    Warp class for a warps plugin for Spigot 1.8

---
+++ Warp.java
@@ -0,0 +1,45 @@
+package warps;
+
+import java.util.Date;
+
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Warp {
+
+	private Location warploc = null;
+	private String creator = null;
+	private long lastused = 0;
+	
+	public Warp(CommandSender sender){
+		//should not be modified after construction
+		creator = sender.getName();
+		Date d = new Date();
+		//will be modified
+		lastused = d.getTime();
+		Player player1 = (Player) sender;
+		//should not be modified after creation
+		warploc = player1.getLocation();
+	}
+	
+	public Long getLastUsed(){
+		
+		return this.lastused;
+	}
+	//Sets date/time of last use
+	public void setLastUsed(){
+		Date d = new Date();
+		this.lastused = d.getTime();
+	}
+	
+	public String getCreator(){
+		
+		return this.creator;
+	}
+	
+	public Location getLocation(){
+		
+		return this.warploc;
+	}
+}
